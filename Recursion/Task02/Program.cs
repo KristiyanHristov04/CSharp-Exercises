ce9eaf276a3063ed52e5be50de0cd595c10f71cc
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-30 12:28:04 +0300

    .

---
+++ Recursion/Task02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(PrintNumbersReversed(number, 1));
+        }
+        static int PrintNumbersReversed(int currNumber, int minNumber)
+        {
+            if (currNumber == minNumber)
+            {
+                return minNumber;
+            }
+            Console.Write(currNumber + " ");
+            return PrintNumbersReversed(currNumber - 1, minNumber);
+        }
+    }
+}
