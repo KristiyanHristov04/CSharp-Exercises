5bb09a09d7fb87acf8ec55e15805e7ba3b8881ac
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-30 12:28:21 +0300

    .

---
+++ Recursion/Task05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            Console.WriteLine(PrintDigitsCount(number, count));
+        }
+        static int PrintDigitsCount(int number, int count)
+        {
+            if (number < 10)
+            {
+                count++;
+                return count;
+            }    
+            count++;
+            return PrintDigitsCount(number / 10, count);
+        }
+    }
+}
