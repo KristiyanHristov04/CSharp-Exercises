5a3e59c56574a571aaa3db86fe64f484d9e47957
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-30 12:28:09 +0300

    .

---
+++ Recursion/Task03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int startingIndex = 1;
+            int sum = 0;
+            Console.WriteLine(SumNumbers(startingIndex, number, sum));
+        }
+        static int SumNumbers(int currNumber, int maxNumber, int sum)
+        {
+            if (currNumber > maxNumber)
+            {
+                return sum;
+            }
+            sum += currNumber;
+            return SumNumbers(currNumber + 1, maxNumber, sum);
+        }
+    }
+}
