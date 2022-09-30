1ed0ca9301a4991adb01ef176f5764c5523c039b
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-09-30 12:27:58 +0300

    .

---
+++ Recursion/Task01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(PrintNumbers(1, number));
+        }
+        static int PrintNumbers(int currNumber, int maxNumber)
+        {
+            if (currNumber == maxNumber)
+            {
+                return maxNumber;
+            }
+            Console.Write(currNumber + " ");
+            return PrintNumbers(currNumber + 1, maxNumber);
+        }
+    }
+}
