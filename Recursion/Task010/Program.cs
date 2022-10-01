5f9b590936c3192e628dd7e776c3d8016351d5b0
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-01 15:25:19 +0300

    .

---
+++ Recursion/Task010/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Fibonacci(number));
+
+            //Fibonacci Sequence(Bonus)
+            for (int i = 1; i <= 10; i++)
+            {
+                int fibonacci = Fibonacci(i);
+                string text = string.Format($"Fibonacci of {i} is {fibonacci}");
+                Console.WriteLine(text);
+            }
+        }
+        private static int Fibonacci(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(n - 2) + Fibonacci(n - 1);
+            }
+        }
+    }
+}
