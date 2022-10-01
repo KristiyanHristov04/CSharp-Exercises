4b3f0869bdeb941823bc90e82fa251e706e64812
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-01 15:25:03 +0300

    .

---
+++ Recursion/Task07/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isPrime = IsPrime(number, 2, 10);
+            if (isPrime)
+            {
+                Console.WriteLine($"{number} is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not a prime number.");
+            }
+        }
+        static bool IsPrime(int number, int startIndex, int endIndex)
+        {
+            if (startIndex == endIndex && number % 10 == 0)
+            {
+                return false;
+            }
+            else if (startIndex == endIndex && number % 10 != 0)
+            {
+                return true;
+            }
+            else if (number % startIndex == 0 && startIndex != number)
+            {
+                return false;
+            }
+            return IsPrime(number, startIndex + 1, endIndex);
+        }
+    }
+}
