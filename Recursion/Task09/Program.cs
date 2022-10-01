add4818660063676c1a530d40fac50f333680a0e
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-10-01 15:25:14 +0300

    .

---
+++ Recursion/Task09/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+        static int Factorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+            return number * Factorial(number - 1);
+        }
+    }
+}
