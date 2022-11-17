2c2397f3e233357590c56eaeb6c778e31739cc1e
Kristiyan Hristov <kristiyan_hristov04@abv.bg>
2022-11-17 20:52:58 +0200

    .

---
+++ Unit Testing/BankAccount/UnitTesting-Exercisev2/BankAccount.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTesting_Exercisev2
+{
+    public class BankAccount
+    {
+        private const double MaxBalance = 10_000;
+        private string firstName;
+        public string FirstName 
+        { 
+            get { return firstName; }
+            protected set
+            {
+                if (value.Length < 4)
+                {
+                    throw new ArgumentException("First name must contains at least 4 characters!");
+                }
+                this.firstName = value;
+            }
+        }
+        private string lastName;
+        public string LastName
+        { 
+            get { return lastName; }
+            protected set
+            {
+                if (value.Length < 4)
+                {
+                    throw new ArgumentException("Last name must contains at least 4 characters!");
+                }
+                this.lastName = value;
+            }
+        }
+
+        private double balance;
+
+        public double Balance
+        {
+            get 
+            { 
+                return balance;
+            }
+            private set
+            {
+                this.balance = value;
+            }
+        }
+        public BankAccount(string firstName, string lastName, double balance)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Balance = balance;
+        }
+
+        public void AddBalance(double amount)
+        {
+            if (this.Balance + amount > MaxBalance)
+            {
+                throw new InvalidOperationException("You don't have enough space to store the money!");
+            }
+            this.Balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if (this.Balance - amount < 0 && amount <= 10_000)
+            {
+                throw new InvalidOperationException("You don't have enough money to withdraw!");
+            }
+
+            if (amount > 10_000)
+            {
+                throw new ArgumentException("You've exceeded the maximum limit!");
+            }
+
+            this.Balance -= amount;
+        }
+    }
+}
---
+++ Unit Testing/BankAccount/UnitTesting-Exercisev2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTesting_Exercisev2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
